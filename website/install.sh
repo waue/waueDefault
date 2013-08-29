e029f209357055eb3a8a7c5d2a5df379b6134b2d
Martin Waue <info@waue.net>
2013-08-29 13:12:09 +0200

    mit erstem install shellscript

---
+++ website/install.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+#####################
+# FUNCTIONS
+#####################
+installjam (){
+echo -n "Install $1? Yes[Enter] No[n]:"
+	#echo -n "Yes: Most Recent Version[Enter] or Version [x.x.x] No: [n]"
+    read v
+    case "$v" in
+    *.*.* ) 
+    jam lock jquery@1.8.x
+    jam install $1 
+    ;;
+    '' ) 
+    	jam install $1;;
+    [n,N] ) 
+    	;;
+    * ) 
+		 echo Wrong input - try again!
+		 a=;;
+    esac
+}
+
+
+
+##################
+# RUN ------------
+##################
+
+######
+#JAM http://jamjs.org
+######
+#test ob jam installiert ist
+cd ./js
+if ! which  jam >/dev/null; then
+    echo 'Error: Jam is not installed (http://jamjs.org) - Abbording'
+#jam ist installiert->pakete werden installiert (jquery, modernizr, ...)
+else
+	#installjam jquery
+	#installjam modernizr
+	echo -n "Install another package? Yes[Packagename]/NO[n]"
+	read p
+	if [ ${p} != "n" ];	then
+	 installjam $p 
+	fi
+fi
+
+
+##########
+# Compass
+##########
+cd ../css
+if ! which  compass >/dev/null; then
+    echo 'Error: Compass is not installed (http://compass-style.org/) - Abbording'
+#compass ist installiert->wird initziert
+else
+echo -n "Init compass? Yes[]/NO[n]"
+	read p
+	if [ ${p} != "n" ];	then
+	 	compass  init > /dev/null
+		#mv styles.scss sass/screen.scss
+	fi
+	#todo:jetzt müsste noch eine default struktur ins sass verzeichnis kopiert werden
+exit
+
+
+
+a=
+while [ -z $a ]; do
+	a=false
+	installjam
+ done   
+fi
+
+
